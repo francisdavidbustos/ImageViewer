94df046a4c205e63d7f196ecb0f282dafa0edb89
Francis David Bustos <francisdavidbustos@gmail.com>
2020-08-11 19:25:39 -0700

    Detail Screen

---
+++ ImageViewer/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  ImageViewer
+//
+//  Created by Francis David Bustos on 8/11/20.
+//  Copyright © 2020 Francis David Bustos. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
